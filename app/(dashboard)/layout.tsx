9800e88bc71df1fd6394d0caeed80ad0e2831714
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-10 23:06:07 +0500

    Create Dashboard layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/dashboard/sidebar/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <div className="w-fit flex items-center gap-2 my-4 px-4">
+          <SidebarTrigger className="-ml-1" />
+        </div>
+        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
