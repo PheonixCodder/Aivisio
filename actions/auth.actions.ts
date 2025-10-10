f86dda37e09d18aaf49fed35bae79357ff36aca7
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-10 23:04:42 +0500

    Create auth.actions.ts

---
+++ actions/auth.actions.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { ROUTES } from "@/lib/routes";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export async function signUp(formData: FormData): Promise<AuthResponse> {
+    const supabase = await createClient();
+
+    const data = {
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
+        options : {
+            data: {
+                name: formData.get("name")
+            }
+        }
+    }
+
+    const { data: user, error } = await supabase.auth.signUp(data)
+
+    return { error: error?.message || "Error signing up", success: !error, data: user || null}
+
+}
+
+export async function logIn(formData: FormData): Promise<AuthResponse> {
+    const supabase = await createClient();
+
+    const data = {
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
+    }
+
+    const { data: user, error } = await supabase.auth.signInWithPassword(data)
+
+    return { error: error?.message || "Error Logging in", success: !error, data: user || null}
+
+}
+
+export async function logOut() : Promise<void> {
+    const supabase = await createClient();
+    await supabase.auth.signOut()
+    redirect(ROUTES.LOGIN)
+}
