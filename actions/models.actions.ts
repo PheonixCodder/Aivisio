e6c3d85cc31cb78288499d74f27d9b632ff221e9
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-10 23:07:45 +0500

    Create models.actions.ts

---
+++ actions/models.actions.ts
@@ -0,0 +1,69 @@
+"use server";
+import { supabaseAdmin } from "@/lib/supabase/admin";
+import { createClient } from "@/lib/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export async function getPresignedStorageUrl(filePath: string){
+    const supabase = await createClient();
+    const { data: { user } } = await supabase.auth.getUser();
+
+    const { data: urlData, error } = await supabaseAdmin.storage.from("training_data").createSignedUploadUrl(`${user?.id}/${new Date().getTime()}_${filePath}`);
+
+    return {
+        signUrl: urlData?.signedUrl || "",
+        error: error?.message || null,
+    }
+}
+
+export async function fetchModels(){
+    const supabase = await createClient();
+    const { data: { user } } = await supabase.auth.getUser();
+
+    const { data, error, count } = (await supabase.from("models").select("*", { count: "exact" }).eq("user_id", user?.id).order("created_at", { ascending: false }));
+
+
+    return { error: error?.message || null, success: !error, data: data || null, count: count || 0 }
+}
+
+export async function deleteModel(id: number, model_id: string, model_version: string){
+        const supabase = await createClient();
+
+    if (model_version){
+        try {
+            const res = await fetch(`https://api.replicate.com/v1/models/pheonixcodder/${model_id}/versions/${model_version}`, { method: "DELETE", headers: { "Authorization": `Bearer ${process.env.REPLICATE_API_TOKEN}` } });
+            if(!res.ok){
+                throw new Error("Error deleting model version");
+            }
+        } catch (error) {
+            console.log("Failed to delete model version", error);
+            const errorMessage = error instanceof Error ? error.message : "Error deleting model";
+            return {
+                error: errorMessage,
+                success: false,
+            }
+        }
+    }
+    if (model_id){
+        try {
+            const res = await fetch(`https://api.replicate.com/v1/models/pheonixcodder/${model_id}`, { method: "DELETE", headers: { "Authorization": `Bearer ${process.env.REPLICATE_API_TOKEN}` } });
+            if(!res.ok){
+                throw new Error("Error deleting model");
+            }
+        } catch (error) {
+            console.log("Failed to delete model", error);
+            const errorMessage = error instanceof Error ? error.message : "Error deleting model";
+            return {
+                error: errorMessage,
+                success: false,
+                data: null
+            }
+        }
+    }
+
+    revalidatePath("/models");
+
+    const { error } = await supabase.from("models").delete().eq("id", id)
+
+    return { error: error?.message || null, success: !error }
+
+}
