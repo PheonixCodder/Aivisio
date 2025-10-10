422e05a3170423b2cdb1f46fefdd0d094e64e8a5
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-10 23:11:19 +0500

    Create Testimonials.tsx

---
+++ components/auth/testimonials/Testimonials.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div>
+      <blockquote className="space-y-2">
+        <p className="text-lg">
+          &ldquo;Aivisio is a game changer for me. I have been able to generate
+          high quality professional headshots within minutes. It has saved me
+          countless hours of work and cost as well.&rdquo;
+        </p>
+        <footer className="text-sm">David S.</footer>
+      </blockquote>
+    </div>
+  );
+};
+
+export default Testimonials;
