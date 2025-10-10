fd992fa4578f5cd3b14f06ee9944297f6df19d01
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-10 23:10:27 +0500

    Setup Global Routes

---
+++ lib/routes.ts
@@ -0,0 +1,17 @@
+export const ROUTES = {
+    HOME: '/',
+    ABOUT: '/about',
+    CONTACT: '/contact',
+    FAQ: '/faq',
+    PRIVACY: '/privacy',
+    TERMS: '/terms',
+    LOGIN: '/login',
+    SIGNUP: '/signup',
+    DASHBOARD: '/dashboard',
+    BILLING: '/billing',
+    SETTINGS: '/settings',
+    IMAGE_GENERATION : '/image-generation',
+    MODEL_TRAINING : '/model-training',
+    MODELS: '/models',
+    GALLERY : '/gallery'
+}
