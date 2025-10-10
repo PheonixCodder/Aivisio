74ad9726b0ae783a84eb817833b4f1b7e9996b0b
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-10 23:11:06 +0500

    Create zustand store to handle global images

---
+++ store/useGeneratedStore.tsx
@@ -0,0 +1,39 @@
+import { generateImageAction, StoreImageInput, storeImages } from "@/actions/image.actions";
+import { ImageFormSchema } from "@/lib/validations";
+import { toast } from "sonner";
+import z from "zod";
+import { create } from "zustand";
+
+const useGeneratedStore = create<GenerateState>((set) => ({
+    loading: false,
+    images: [],
+    error: null,
+
+    generateImage: async (input: z.infer<typeof ImageFormSchema>) => {
+        set({loading: true, error: null});
+
+        const toastId = toast.loading("Generating images...");
+
+        try {
+            const { error, success, data } = await generateImageAction(input);
+            if (!success){
+                set({loading: false, error: error});
+                return
+            }
+            const dataWithUrl = data!.map(url => ({url: url as string, ...input}));
+
+            set({images: dataWithUrl as { url: string; }[] | undefined, loading: false});
+
+            toast.success("Images generated successfully", {id: toastId});
+            
+            await storeImages(dataWithUrl as StoreImageInput)
+
+            toast.success("Images stored successfully", {id: toastId});
+        } catch (error) {
+            set({loading: false, error: error as string});
+            toast.error(error as string, {id: toastId});
+        }
+    }
+}));
+
+export default useGeneratedStore
