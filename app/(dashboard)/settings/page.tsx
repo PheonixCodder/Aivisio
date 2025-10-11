b9c8956c26181004c0ecc879b034892e588e3fd9
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-11 22:35:29 +0500

    Update Settings Page

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,10 +1,25 @@
+import AccountForm from '@/components/dashboard/settings/AccountForm'
+import SecurityForm from '@/components/dashboard/settings/SecurityForm';
+import { getUser } from '@/lib/supabase/queries';
+import { createClient } from '@/lib/supabase/server';
 import React from 'react'
 
-const Settings = () => {
+const Settings = async () => {
+  const supabase = await createClient();
+  const user = await getUser(supabase);
   return (
-    <div>
-      
-    </div>
+    <section className='container mx-auto space-y-8'>
+      <div>
+        <h1 className='text-3xl font-bold tracking-tight'>Account Settings</h1>
+        <p className='text-muted-foreground'>
+          Manage your account settings and personal information
+        </p>
+      </div>
+      <div className='grid gap-8'>
+        <AccountForm user={user!} />
+        <SecurityForm user={user!} />
+      </div>
+    </section>
   )
 }
 
