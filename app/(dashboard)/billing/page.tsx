ecaff38d735805f2fea6299335897379b7625944
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-11 22:33:56 +0500

    Completed Billing Page

--- app/(dashboard)/billing/page.tsx
+++ app/(dashboard)/billing/page.tsx
@@ -1,16 +1,20 @@
+import { getCredits } from '@/actions/credit.actions';
 import PlanSummary from '@/components/dashboard/billing/PlanSummary';
+import Pricing from '@/components/dashboard/billing/Pricing';
 import { getProducts, getSubscription, getUser } from '@/lib/supabase/queries'
 import { createClient } from '@/lib/supabase/server';
 import React from 'react'
 
 const Page = async () => {
-    const supabase = await createClient();
+    const supabase = await createClient();    
   
-  const [user, products, subscription] = await Promise.all([
+  const [user, products, subscription, { data: credits}] = await Promise.all([
       getUser(supabase),
       getProducts(supabase),
       getSubscription(supabase),
+      getCredits()
     ])
+    
     
   return (
     <section className='container mx-auto space-y-8'>
@@ -21,7 +25,12 @@ const Page = async () => {
         </p>
       </div>
       <div className='grid gap-10'>
-        <PlanSummary subscription={subscription} user={user} products={products}  />
+        <PlanSummary credits={credits!} subscription={subscription} user={user} products={products!}  />
+        {
+          subscription.status === 'active' && (
+            <Pricing subscription={subscription} showInterval={false} user={user} products={products!} activeProduct={subscription.prices.products.name.toLowerCase() || "pro"} />
+          )
+        }
       </div>
     </section>
   )
