c827835ec0715e605a68470737c8aa0cd1a5b6d3
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-11 22:35:17 +0500

    Update model-training page

--- app/(dashboard)/model-training/page.tsx
+++ app/(dashboard)/model-training/page.tsx
@@ -1,14 +1,16 @@
-import ModelTrainingForm from '@/components/dashboard/model-training/forms/ModelTraining'
-import React from 'react'
+import ModelTrainingForm from "@/components/dashboard/model-training/forms/ModelTraining";
+import React from "react";
 
 const ModelTraining = () => {
   return (
-    <section className='container mx-auto' >
-      <h1 className='text-3xl font-bold mb-2'>Train Model</h1>
-      <p className='text-sm text-muted-foreground mb-6'>Train a new model with your own images</p>
-      <ModelTrainingForm />
+    <section className="container mx-auto w-full flex items-center justify-center flex-col">
+      <h1 className="text-3xl font-bold mb-2">Train Model</h1>
+      <p className="text-sm text-muted-foreground mb-6">
+        Train a new model with your own images
+      </p>
+        <ModelTrainingForm />
     </section>
-  )
-}
+  );
+};
 
-export default ModelTraining
+export default ModelTraining;
